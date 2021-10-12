c749b1f7e2f81f806faecf3bb1f468a5e4b12328
Nikita <nsotnikov16@yandex.ru>
2021-10-12 10:00:34 +0500

    commit

---
+++ fls-gulp/js/script.js
@@ -0,0 +1,54 @@
+
+// Двухуровневое меню
+const twoLevelMenus = document.querySelectorAll('.menu__item-dropdown')
+twoLevelMenus.forEach((item, ind, arr) => {
+    item.querySelector('.menu__link').addEventListener('click', () => {
+        item.classList.contains('open') ? item.classList.remove('open') : item.classList.add('open')
+        arr.forEach(el => el !== item ? el.classList.remove('open') : "")
+    })
+})
+
+
+// Меню модалка
+const menuBtn = document.querySelector('.burger')
+const menu = document.querySelector('.menu')
+const banner = document.querySelector('.banner')
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('burger_open')
+    menu.classList.toggle('menu_open')
+    if(banner) banner.classList.toggle('bc-white');
+})
+
+
+// Поиск
+
+const inputSearch = document.querySelector('.search input')
+const searchClose = document.querySelector('.search__close')
+const formSearch = document.querySelector('.search')
+
+inputSearch.addEventListener('input', (evt) => evt.target.value ? evt.target.parentNode.classList.add('input-value') : evt.target.parentNode.classList.remove('input-value'))
+searchClose.addEventListener('click', () => {
+    formSearch.classList.remove('open')
+    formSearch.reset();
+})
+
+const appendSearch = () => $('.search-and-phone').append($('.search'))
+const searchInHeader = () => $('.search').insertAfter($('.logo_header'))
+const logicSearch = () => {
+        $('.search-img').click(function() {
+            $('.search').addClass('open')
+        })
+}
+const functional = () => {
+    $(window).resize(function () {
+        window.innerWidth <= 1200 ? appendSearch() : searchInHeader()
+        if(window.innerWidth < 1200) logicSearch()
+    })
+} 
+
+const searchMin = () => {appendSearch(); logicSearch(); functional()}
+
+window.innerWidth <= 1200 ? searchMin() : functional()
+
+//document.addEventListener('click', (evt) => (evt.target !== formSearch) && (!formSearch.classList.contains('open')) ? formSearch.classList.remove('open') : '')
